0bf2fdf3a8dae0d128e2637ba1006f5b27e7e4e6
Kevin Kelley <kevin@kelleysoft.com>
2019-06-25 15:16:57 +0800

    Message marshal with tests

--- src/encoding/marshal.rs
+++ src/encoding/marshal.rs
@@ -26,20 +26,77 @@
 // 	), nil
 // }
 
-use super::message::{Message};
+pub use super::message::{Message};
 
-pub fn marshal(_msg: Message) -> Option<String> {
-    return Some("ok".to_string())
+pub fn marshal(msg: Message) -> Option<Vec<u8>> {
+    assert!(msg.version == "0.2");
+    assert!(msg.protocol != "");
+
+    let header: String = format!("EWP {} {} {} {}\n",
+                                msg.version,
+                                msg.protocol,
+                                msg.header.len(),
+                                msg.body.len());
+
+    let mut outbytes: Vec<u8> = header.into_bytes();
+    outbytes.extend(msg.header);
+    outbytes.extend(msg.body);
+
+    return Some(outbytes)
 }
 
 
 
 #[cfg(test)]
 mod tests {
+    use super::{Message, marshal};
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn basic_sanity() {
+
+        //   - desc: 'no body'
+        //     marshalled: "EWP 0.2 PING 0 0\n"
+        let mut msg = Message::new("PING", &vec!(), &vec!());
+        assert_eq!(marshal(msg).unwrap(), "EWP 0.2 PING 0 0\n".as_bytes());
+        //   - desc: '10 byte body'
+        //     marshalled: "EWP 0.2 PING 0 10\n0123456789"
+        msg = Message::new("PING", &vec!(), "0123456789".as_bytes());
+        assert_eq!(marshal(msg).unwrap(), "EWP 0.2 PING 0 10\n0123456789".as_bytes());
+        //   - desc: '10 byte header'
+        //     marshalled: "EWP 0.2 PING 10 0\n0123456789"
+        msg = Message::new("PING", "0123456789".as_bytes(), &vec!());
+        assert_eq!(marshal(msg).unwrap(), "EWP 0.2 PING 10 0\n0123456789".as_bytes());
+        //   - desc: '9 byte header, 10 byte body'
+        //     marshalled: "EWP 0.2 PING 9 10\n9876543210123456789"
+        msg = Message::new("PING", "987654321".as_bytes(), "0123456789".as_bytes());
+        assert_eq!(marshal(msg).unwrap(), "EWP 0.2 PING 9 10\n9876543210123456789".as_bytes());
+        //   - desc: '9 byte header, 10 byte body, extra newlines'
+        //     marshalled: "EWP 0.2 PING 9 10\n\n876543210123456\n89"
+        msg = Message::new("PING", "\n87654321".as_bytes(), "0123456\n89".as_bytes());
+        assert_eq!(marshal(msg).unwrap(), "EWP 0.2 PING 9 10\n\n876543210123456\n89".as_bytes());
+        //   - desc: '9 byte header, 10 byte body, extra extra newlines'
+        //     marshalled: "EWP 0.2 PING 9 10\n\n87654321\n\n\n\n\n\n\n\n\n\n"
+        msg = Message::new("PING", "\n87654321".as_bytes(), "\n\n\n\n\n\n\n\n\n\n".as_bytes());
+        assert_eq!(marshal(msg).unwrap(), "EWP 0.2 PING 9 10\n\n87654321\n\n\n\n\n\n\n\n\n\n".as_bytes());
+        //   - desc: '9 byte header, 10 byte body, control character montage'
+        //     marshalled: "EWP 0.2 PING 9 10\n\n87654321\n\0\a\b\f\n\r\t\v\\"
+        // NOTE: those aren't valid Rust control characters...
+        msg = Message::new("PING", "\n87654321".as_bytes(), "\n\0\x0a\x0b\x0f\n\r\t\x01\\".as_bytes());
+        assert_eq!(marshal(msg).unwrap(), "EWP 0.2 PING 9 10\n\n87654321\n\0\x0a\x0b\x0f\n\r\t\x01\\".as_bytes());
+
     }
+
+    // - suite: 'test different commands'
+    //   requests:
+    //   - desc: 'PING'
+    //     marshalled: "EWP 0.2 PING 0 0\n"
+    //   - desc: 'FOO'
+    //     marshalled: "EWP 0.2 FOO 0 0\n"
+    //   - desc: 'BAR'
+    //     marshalled: "EWP 0.2 BAR 0 0\n"
+    //   - desc: 'PONG'
+    //     marshalled: "EWP 0.2 PONG 0 0\n"
+    //   responses: []
 
 
     //
--- src/encoding/message.rs
+++ src/encoding/message.rs
@@ -14,9 +14,59 @@
 // 	Body     []byte
 // }
 
+#[derive(Clone, Hash, Default, PartialEq, Debug)]
 pub struct Message {
     pub version: String,
     pub protocol: String,
     pub header: Vec<u8>,
     pub body: Vec<u8>,
 }
+
+impl Message {
+
+    pub fn new(proto: &str, hdr: &[u8], bdy: &[u8]) -> Message {
+        return Message {
+            version: "0.2".to_string(),
+            protocol: proto.to_string(),
+            header: hdr.to_vec(),
+            body: bdy.to_vec()
+        }
+    }
+
+}
+
+// func reqParse(req string) Request {
+// 	res := strings.Split(req, "\n")
+//
+// 	reqLine := res[0]
+// 	payload := strings.Join(res[1:], "\n")
+// 	r := strings.Split(reqLine, " ")
+// 	if len(r) < 8 {
+// 		r = append(r, " ")
+// 	}
+// 	headersLen, _ := strconv.Atoi(r[3])
+// 	bodyLen, _ := strconv.Atoi(r[4])
+// 	headers := payload[0:headersLen]
+// 	body := payload[headersLen : headersLen+bodyLen]
+//
+// 	request := Request{
+// 		proto:   r[0],
+// 		version: r[1],
+// 		command: r[2],
+// 		headers: []byte(headers),
+// 		body:    []byte(body),
+// 	}
+// 	return request
+// }
+//
+// func reqMarshal(req Request) string {
+// 	requestLine := fmt.Sprintf("%s %s %s %d %d",
+// 		req.proto,
+// 		req.version,
+// 		req.command,
+// 		len(req.headers),
+// 		len(req.body))
+//
+// 	r := fmt.Sprintf("%s\n%s%s", requestLine, string(req.headers), string(req.body))
+// 	return r
+// }
